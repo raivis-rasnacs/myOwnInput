79f06b2263d5f48436c74f7645df0324e572e45f
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-07-07 06:54:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+class myInput():
+    def __init__(self, text):
+      print(text)
+      temp_value = input()
+      for symbol in temp_value:
+          if not symbol.isdigit():
+              self.value = str(temp_value)
+              break
+      else:
+          self.value = int(temp_value)
+
+    def __str__(self):
+      return str(self.value)
+        
+def mytype(var):
+  return type(var.value)
+
+#########
+
+a = myInput("Ievadi datus!")
+print(a)
+print(mytype(a))
